c98956fbb17342ebcc071a021fab329004aca061
beautifulface82 <beutysujin@gmail.com>
2026-06-22 11:14:37 +0900

    Refactor main.jsx to include routing logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import BeautyConsultation from './BeautyConsultation.jsx'
+
+function Root() {
+  const [page, setPage] = useState(window.location.hash)
+
+  useEffect(() => {
+    const onHash = () => setPage(window.location.hash)
+    window.addEventListener('hashchange', onHash)
+    return () => window.removeEventListener('hashchange', onHash)
+  }, [])
+
+  if (page === '#/beauty') return <BeautyConsultation />
+  return <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
+
 )
